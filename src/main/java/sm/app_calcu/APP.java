2400238d2d343edb7be77efc7cc81ef047485e66
Moises Aguilar <moises.aguilar.et7@gmail.com>
2024-07-17 16:25:45 -0300

    vinculos con la carpeta de estilos

--- src/main/java/sm/app_calcu/APP.java
+++ src/main/java/sm/app_calcu/APP.java
@@ -7,12 +7,14 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class APP extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(APP.class.getResource("vista.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 517, 621);
+        scene.getStylesheets().add(getClass().getResource("/sm/app_calcu/Estilos/botones.css").toExternalForm());
+        stage.setResizable(false);
+        stage.setTitle("Calculadora");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/sm/app_calcu/Controlador.java
@@ -0,0 +1,14 @@
+package sm.app_calcu;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controlador {
+
+
+
+
+
+
+
+}
